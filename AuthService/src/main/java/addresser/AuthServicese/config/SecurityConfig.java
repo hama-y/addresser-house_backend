1a5172b4cdcaa50a890586b5caddc9b02861c468
Shogun-yuki <yuuuuuki1123@docomo.ne.jp>
2023-01-15 17:27:46 +0900

    Add:SecurityConfig

---
+++ AuthService/src/main/java/addresser/AuthServicese/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package addresser.AuthServicese.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+}
