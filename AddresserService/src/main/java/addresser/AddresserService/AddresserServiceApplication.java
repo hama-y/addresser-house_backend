02b348e46c417d461ba98a4bb49dd7641fed66a7
Shota-Yukawa <71016455+Shota-Yukawa@users.noreply.github.com>
2023-03-05 18:53:04 +0900

    AddresserServiceの追加。maven install　済み

---
+++ AddresserService/src/main/java/addresser/AddresserService/AddresserServiceApplication.java
@@ -0,0 +1,13 @@
+package addresser.AddresserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddresserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddresserServiceApplication.class, args);
+	}
+
+}
---
+++ AddresserService/src/test/java/addresser/AddresserService/AddresserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package addresser.AddresserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddresserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
